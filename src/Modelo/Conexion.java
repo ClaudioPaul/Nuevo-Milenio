7c7a8efb665490cf88f38626427d02246da7da07
ClaudioPaul <claudiopaulce@gmail.com>
2021-12-16 01:22:52 -0500

    Se Añadio Controladores

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -16,6 +16,7 @@ import java.sql.SQLException;
 public class Conexion {
      public  Connection getConexion() throws ClassNotFoundException{
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        
         String url = "jdbc:sqlserver://elhidaja-server.database.windows.net\\elhidaja@elhidaja-server:1433;databaseName=NuevoMilenio;user=elhidaja;password=Setiembre19977";
         try {
             Connection con = DriverManager.getConnection(url);
